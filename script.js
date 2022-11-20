cdc272eb904ea9f328a1c459c6742def16d230b3
Carlos Rodriguez <105572165+SuperAorus@users.noreply.github.com>
2022-11-19 20:30:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,27 +4,27 @@ const secondEl = document.getElementById("seconds");
 const ampmEl = document.getElementById("ampm");
 
 function updateClock() {
-    let h = new Date().getHours();
-    let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
-    let ampm = "AM";
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
 
-    if (h > 12) {
-        ampm = "PM"
-    }
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
 
-    h = h<10 ? "0" + h : h
-    m = m<10 ? "0" + m : m
-    s = s<10 ? "0" + s : s
-    
-    hourEl.innerText = h;
-    minuteEl.innerText = m;
-    secondEl.innerText = s;
-    ampmEl, (innerText = ampm);
-    setTimeout(()=>{
-        updateClock()
-    }, 1000)
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl.innerText = ampm;
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
 }
 
-
-updateClock()
+updateClock();
